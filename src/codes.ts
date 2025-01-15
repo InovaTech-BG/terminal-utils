435fa9a4319072c54944f0ccc45c78f8845a1eca
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2025-01-15 11:36:52 -0300

    INiciando projeto

---
+++ src/codes.ts
@@ -0,0 +1,13 @@
+export const codes: Record<string, string> = {
+	reset: "0",
+	bold: "1",
+	light: "2",
+	italic: "3",
+	underline: "4",
+	slowBlink: "5",
+	rapidBlink: "6",
+	reverseVideo: "7",
+	conceal: "8",
+	crossedOut: "9",
+	primaryFont: "10",
+};
